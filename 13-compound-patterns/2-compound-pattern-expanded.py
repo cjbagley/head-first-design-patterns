e42cdccf02d6a51eedefb3375a4cd716341de347
Colin Bagley <cjbagley@googlemail.com>
2024-11-04 11:55:08 +0000

    feat: Add Compound pattern

--- 13-compound-patterns/2-compound-pattern-expanded.py
+++ 13-compound-patterns/2-compound-pattern-expanded.py
@@ -1,7 +1,6 @@
 from quackable import Quackable
 from factories import AbstractDuckFactory, CountingDuckFactory, QuackCounter
 
-
 """
 Adapter pattern
 """
@@ -21,6 +20,22 @@ class GooseAdapter(Quackable):
 
 
 """
+Composite pattern
+"""
+
+
+class Flock(Quackable):
+    quackers = []
+
+    def add(self, quacker: type(Quackable)):
+        self.quackers.append(quacker)
+
+    def quack(self):
+        for quacker in self.quackers:
+            quacker.quack()
+
+
+"""
 Program
 """
 
@@ -30,7 +45,7 @@ def simulate(duck: type(Quackable)) -> None:
 
 
 if __name__ == "__main__":
-    factory: AbstractDuckFactory = CountingDuckFactory
+    factory: type(AbstractDuckFactory) = CountingDuckFactory
     mallard_duck = factory.create_mallard_duck()
     redhead_duck = factory.create_redhead_duck()
     duck_call = factory.create_duck_call()
@@ -41,7 +56,13 @@ if __name__ == "__main__":
     simulate(duck_call)
     simulate(rubber_duck)
 
-    goose = GooseAdapter(Goose())
-    simulate(goose)
+    adapted_goose = GooseAdapter(Goose())
+    simulate(adapted_goose)
 
+    print(QuackCounter.get_quack_count())
+
+    flock = Flock()
+    flock.add(redhead_duck)
+    flock.add(duck_call)
+    flock.quack()
     print(QuackCounter.get_quack_count())
